172651b4663928d71b3d3d37546c46aa0ba1aac7
Marcelo Collares <mmcollares@gmail.com>
2024-08-29 16:04:11 -0300

    Alterando Alteracoes

--- Main.java
+++ Main.java
@@ -10,27 +10,27 @@ public class Main {
     public static void main(String[] args) {
         try {
             // Construção da URI com query parameters
-            String uri = "Teste";
+            String uri = "Teste de URI";
 
             // Criação do cliente HTTP
             HttpClient client = HttpClient.newHttpClient();
 
             // Construção do corpo da requisição
-            String jsonBody = "{ \"username\": \"73612754009\" }";
+            String jsonBody = "{ \"username\": \"12345\" }";
 
             // Construção da requisição HTTP DELETE
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(uri))
                     .header("Authorization", "oi")
                     .header("Content-Type", "application/json")
-                    .method("DELETE", BodyPublishers.ofString(jsonBody))
+                    .method("POST", BodyPublishers.ofString(jsonBody))
                     .build();
 
             // Envio da requisição e obtenção da resposta
             HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
 
             // Impressão do status e do corpo da resposta
-            System.out.println("Status Code: " + response.statusCode());
+            System.out.println("Status Code Default: " + response.statusCode());
             System.out.println("Response Body: " + response.body());
         } catch (Exception e) {
             e.printStackTrace();
